5a601fffc2cfd81f5361ac18ba75422febafacb6
purgalo <olasz.g48@gmail.com>
2025-11-12 15:02:34 +0100

    megirtam a logikat a regisztraciohoz

--- fullstack-blog-backend/app/Http/Controllers/api/AuthController.php
+++ fullstack-blog-backend/app/Http/Controllers/api/AuthController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        return 'works';
+        $data = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'password' => ['required','min:8', 'regex:/[A-Z]/', 'unique:users', 'confirmed'],
+        ]);
+
+        $user = User::create($data);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
     }
 
     public function login(Request $request){
