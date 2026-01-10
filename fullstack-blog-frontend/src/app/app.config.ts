b42aa3ef8b278c5a3f345b5f39646de01ecfd2d1
purgalo <olasz.g48@gmail.com>
2026-01-10 15:53:57 +0100

    megcsinaltam frontenden is a roleok elkuloniteset

--- fullstack-blog-frontend/src/app/app.config.ts
+++ fullstack-blog-frontend/src/app/app.config.ts
@@ -1,14 +1,35 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection,
+  provideAppInitializer, inject
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { Auth } from './services/auth';
+import { tap, of, catchError } from 'rxjs';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    provideAppInitializer(() => {
+      const authService = inject(Auth);
+      const token = authService.getToken();
+
+      if (token) {
+        return authService.getProfile().pipe(
+          catchError((error) => {
+            authService.clearToken();
+            return of(null);
+          })
+        );
+      }
+      
+      return of(null);
+    })
   ]
 };
